26e3549ce52014b586c3fefbd16c6297716bc299
Jeff Church <jeffmichaelchurch@gmail.com>
2025-11-07 11:32:26 -0800

    header adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import ViewPage from './pages/ViewPage'
 import EditorPage from './pages/EditorPage'
 
 function App() {
+  const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
   return (
-    <Router>
+    <Router basename="/HowDoYouKnowThem">
       <div className="app">
-        <header className="app-header">
-          <h1>How Do You Know Them?</h1>
-          <nav className="app-nav">
-            <Link to="/" className="nav-link">View</Link>
-            <Link to="/edit" className="nav-link">Edit</Link>
-          </nav>
-        </header>
+        {isLocalhost && (
+          <header className="app-header">
+            <h1>How Do You Know Them?</h1>
+            <nav className="app-nav">
+              <Link to="/" className="nav-link">View</Link>
+              <Link to="/edit" className="nav-link">Edit</Link>
+            </nav>
+          </header>
+        )}
         <Routes>
           <Route path="/" element={<ViewPage />} />
           <Route path="/edit" element={<EditorPage />} />
